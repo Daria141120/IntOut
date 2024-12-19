7aea684325b12e0c330cecdec4cc1dc2042aa44c
Dasha <d.bachu7@yandex.ru>
2024-12-19 18:52:59 +0300

    com1

--- src/main/java/org/example/ShowFile.java
+++ src/main/java/org/example/ShowFile.java
@@ -1,4 +1,33 @@
 package org.example;
+import java.io.*;
+
 
 public class ShowFile {
+    public static void main(String[] args)  {
+    int i = 55;
+    double d = 0.1;
+    double dd=0;
+    boolean bool = false;
+
+    try (DataOutputStream dos = new DataOutputStream(new FileOutputStream("myf.txt"))){
+        System.out.println("запись d = "+d);
+        dos.writeDouble(d);
+
+    }catch (IOException e){
+        System.out.println(e);
+    }
+
+
+        try (DataInputStream dis = new DataInputStream(new FileInputStream("myf.txt"))){
+
+           dd = dis.readDouble();
+
+        }catch (IOException e){
+            System.out.println(e);
+            System.out.println(e);
+        }
+        System.out.println("считать dd = "+ dd);
+
+
+    }
 }
