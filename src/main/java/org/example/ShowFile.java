3f865955d17368f40b51ca35d8bb41eb6d1334a7
Dasha <d.bachu7@yandex.ru>
2024-12-20 12:16:54 +0300

    file comparison

--- src/main/java/org/example/ShowFile.java
+++ src/main/java/org/example/ShowFile.java
@@ -1,32 +1,38 @@
 package org.example;
 import java.io.*;
 
+import static java.lang.Math.abs;
+
 
 public class ShowFile {
+
+
     public static void main(String[] args)  {
-    int i = 55;
-    double d = 0.1;
-    double dd=0;
-    boolean bool = false;
+        int i = 0, j = 0;
+        int count = 0;
+        if (args.length != 2)
+            System.out.println("enter names two files");
 
-    try (DataOutputStream dos = new DataOutputStream(new FileOutputStream("myf.txt"))){
-        System.out.println("запись d = "+d);
-        dos.writeDouble(d);
-
-    }catch (IOException e){
-        System.out.println(e);
-    }
-
-
-        try (DataInputStream dis = new DataInputStream(new FileInputStream("myf.txt"))){
-
-           dd = dis.readDouble();
-
-        }catch (IOException e){
-            System.out.println(e);
-            System.out.println(e);
+        try (var fin1 = new FileInputStream(args [0]) ; var fin2 = new FileInputStream(args[1])) {
+            do {
+                i = fin1.read();
+                j = fin2.read();
+                System.out.println("i = "+ i + " j = "+ j);
+                if (!( abs(i - j) == 32 || i - j == 0 ))
+             //   if (i != j)
+                    break;
+                count++;
+            }while (i != -1  &&  j != -1);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-        System.out.println("считать dd = "+ dd);
+
+        if (i != j)
+            System.out.println("file different sinse : " + count);
+        else
+            System.out.println("files similar");
 
 
     }
